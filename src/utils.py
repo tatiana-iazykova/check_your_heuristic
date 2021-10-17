6233fde0b148d71f14a6a5a76ac833c202373c11
Tatiana Iazykova <tania_yazykova@bk.ru>
2021-10-17 22:14:20 +0300

    add load_config function

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,14 @@
 import pandas as pd
 from typing import List
+import yaml
 
 def get_target_list(target_column: pd.Series) -> List:
     return target_column.unique().to_list()
 
 def get_correlated_words(input: pd.DataFrame) -> pd.DataFrame:
     pass
+
+def load_config(path: str):
+    with open("path", "r") as yamlfile:
+        data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+        return data
