97ec9932094576cf5e8cef6af54dc8d645af5ef1
Tatiana Iazykova <tania_yazykova@bk.ru>
2021-10-17 22:16:04 +0300

    PEP8 fix

--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,14 @@ import pandas as pd
 from typing import List
 import yaml
 
+
 def get_target_list(target_column: pd.Series) -> List:
     return target_column.unique().to_list()
 
-def get_correlated_words(input: pd.DataFrame) -> pd.DataFrame:
+
+def get_correlated_words(column: pd.DataFrame) -> pd.DataFrame:
     pass
+
 
 def load_config(path: str):
     with open("path", "r") as yamlfile:
