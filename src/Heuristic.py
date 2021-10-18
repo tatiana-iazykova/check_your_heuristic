1790baad6ad9b3e54afec98bd323b375ed751537
tatiana-iazykova <70767376+tatiana-iazykova@users.noreply.github.com>
2021-10-18 10:24:47 +0300

    get rid of rubbish

--- src/Heuristic.py
+++ src/Heuristic.py
@@ -6,14 +6,6 @@ class BaseHeuristicSolver(BaseSolver):
    
     def __init__(self, path: str, path_valid: str = None):
         super(BaseHeuristicSolver, self).__init__(path, path_valid)
-
-    def preprocess(self, columns: pd.Series):
-        for column in columns:
-            self.train[f"{column}_tokenized"] = self.train[column].apply(self.clean_text)
-            self.valid[f"{column}_tokenized"] = self.valid[column].apply(self.clean_text)
-
-    def clean_text(self, text: str) -> str:
-      return text
     
     def check_heuristics(self):
         pass
