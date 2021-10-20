510e77e9c562eb6f9f4e55496c2fcd146ce8540e
Tatiana Iazykova <tania_yazykova@bk.ru>
2021-10-20 22:59:32 +0300

    add additional argument

--- src/Heuristic.py
+++ src/Heuristic.py
@@ -1,10 +1,10 @@
 from Base import BaseSolver
-import pandas as pd
+from typing import Dict, Any
 
 
 class BaseHeuristicSolver(BaseSolver):
    
-    def __init__(self, path: str, path_valid: str = None):
+    def __init__(self, path: str, config: Dict[str, Any], path_valid: str = None):
         super(BaseHeuristicSolver, self).__init__(path, path_valid)
     
     def check_heuristics(self):
