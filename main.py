83eebe72702d2889aa1be329dd9012c5648aceca
Tatiana Iazykova <tania_yazykova@bk.ru>
2021-10-27 23:06:10 +0300

    update main to instantiate Dataset class

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from src.Heuristic import BaseHeuristicSolver
 from src.utils import load_config
+from src.Dataset import Dataset
 import warnings
 warnings.filterwarnings("ignore")
 
 
 def main():
     config = load_config("config.yaml")
-    MyHeuristicCheck = BaseHeuristicSolver(path=config['train_dataset_dir'],
-                                         path_valid=config['valid_dataset_dir'],
-                                         config=config)
+    dataset = Dataset(path=config['train_dataset_dir'], path_valid=config['valid_dataset_dir'])
+    MyHeuristicCheck = BaseHeuristicSolver(dataset=dataset, config=config)
     MyHeuristicCheck.check_heuristics()
     MyHeuristicCheck.all_methods()
 
