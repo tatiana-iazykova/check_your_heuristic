f0934d2403e3fbde054e006a9de36db7e87c7124
Tatiana Iazykova <tania_yazykova@bk.ru>
2021-10-27 23:29:35 +0300

    PEP8 fix

--- src/Heuristic.py
+++ src/Heuristic.py
@@ -2,6 +2,7 @@ from src.Base import BaseSolver
 from typing import Dict, Any
 from src.Dataset import Dataset
 
+
 class BaseHeuristicSolver(BaseSolver):
    
     def __init__(self, config: Dict[str, Any], dataset: Dataset):
