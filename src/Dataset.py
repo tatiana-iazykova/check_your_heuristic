4580705ac1040dac0bd826cc48c0ceb8888c3eda
Tatiana Iazykova <tania_yazykova@bk.ru>
2021-10-27 23:26:50 +0300

    add docstrings

--- src/Dataset.py
+++ src/Dataset.py
@@ -19,7 +19,7 @@ class Dataset:
         self.valid = self.read_data(Path(path_valid).as_posix()) if path_valid is not None else None
         self.test = self.read_data(Path(path_test).as_posix()) if path_test is not None else None
 
-    def read_data(self, path: str) -> pd.DataFrame:
+    def read_data(self, path: Path) -> pd.DataFrame:
         """
         Given the path to the file returns extension of that file
 
@@ -28,22 +28,37 @@ class Dataset:
         :return: ".csv"
         """
         _, extension = os.path.splitext(path)
-        if extension in self.valid_data_types:
+        if extension.lower() in self.valid_data_types:
             return self.valid_data_types[extension](path)
         else:
             raise ValueError("Data type is not supported, please convert your dataset "
                              f"to one of the following formats {list(self.valid_data_types.keys())}.")
 
     @staticmethod
-    def _read_csv(path: str):
+    def _read_csv(path: Path) -> pd.DataFrame:
+        """
+        Reads a csv file given its path
+        :param path: Path("../../some_file.csv")
+        :return: dataframe
+        """
         return pd.read_csv(filepath_or_buffer=path)
 
     @staticmethod
-    def _read_excel(path: str):
-        return pd.read_excel(io=path)
+    def _read_excel(path: Path) -> pd.DataFrame:
+        """
+        Reads a xls or xlsx file given its path
+        :param path: Path("../../some_file.xlsx")
+        :return: dataframe
+        """
+        return pd.read_excel(io=path, engine="openpyxl")
 
     @staticmethod
-    def _read_json(path: str):
+    def _read_json(path: Path) -> pd.DataFrame:
+        """
+        Reads a json or jsonl file given its path
+        :param path: Path("../../some_file.jsonl")
+        :return: dataframe
+        """
         return pd.read_json(path_or_buf=path, lines=True)
 
 
