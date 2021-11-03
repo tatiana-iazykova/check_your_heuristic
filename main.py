cc2aa1593ad0e42289a905d9e50f68d81744a36b
Tatiana Iazykova <tania_yazykova@bk.ru>
2021-11-03 21:33:11 +0300

    add first heuristic

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.Heuristic import BaseHeuristicSolver
+from heuristics.BasicHeuristics import BasicHeuristics
 from src.utils import load_config
 from src.Dataset import Dataset
 import warnings
@@ -8,7 +8,7 @@ warnings.filterwarnings("ignore")
 def main():
     config = load_config("config.yaml")
     dataset = Dataset(path=config['train_dataset_dir'], path_valid=config['valid_dataset_dir'])
-    MyHeuristicCheck = BaseHeuristicSolver(dataset=dataset, config=config)
+    MyHeuristicCheck = BasicHeuristics(dataset=dataset, config=config)
     MyHeuristicCheck.check_heuristics()
     MyHeuristicCheck.all_methods()
 
