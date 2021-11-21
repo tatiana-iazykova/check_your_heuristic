a5a341be3b98c0469a1d672bc08e604fcd6aae90
Tatiana Iazykova <tania_yazykova@bk.ru>
2021-11-21 23:40:00 +0300

    create RuCoS draft (unworking: NotImplementedError)

--- src/heuristics/ReCoRDHeuristics.py
+++ src/heuristics/ReCoRDHeuristics.py
@@ -9,6 +9,8 @@ import numpy as np
 class ReCoRDHeuristics(BaseHeuristicSolver):
     def __init__(self, config: Dict[str, Any], dataset: ReCoRDDataset):
         super(BaseHeuristicSolver, self).__init__(dataset=dataset, config=config)
+        self.passage_column = config["passage_column"]
+        self.entities_column = config["entities_column"]
 
     @staticmethod
     def normalize_answer(text: str):
@@ -25,33 +27,62 @@ class ReCoRDHeuristics(BaseHeuristicSolver):
 
         return white_space_fix(remove_punct(text.lower()))
 
-    def get_row_pred(row, vect):
-        res = []
+    @staticmethod
+    def _get_entities(row):
         words = [
-            row["passage"]["text"][x["start"]: x["end"]]
-            for x in row["passage"]["entities"]]
-        text = row['passage']['text'].split()
-        for line in row["qas"]:
-            line_candidates = []
-            _words = []
+            row["text"][x["start"]: x["end"]]
+            for x in row["entities"]
+        ]
+        return words
+
+    def filtration_count_heuristic(self, row):
+
+        line_candidates = []
+        _words = []
+        text = row['text'].split()
+        words = self._get_entities(row)
+        for word in words:
+            if word[:-2] not in row['question'] or text.count(words[:-2]) >= 2:
+                _words.append(word)
+
+        if len(_words) == 0:
             for word in words:
-                if word[:-2] not in line['query'] or text.count(words[:-2]) >= 2:
-                    _words.append(word)
-            if len(_words) == 0:
-                for word in words:
-                    line_candidates.append(line["query"].replace("@placeholder", word))
-                pred_idx = np.random.choice(np.arange(1, len(line_candidates)),
-                                            size=1)[0]
-                pred = np.array(words)[pred_idx]
-            elif len(_words) == 1:
-                pred = _words[0]
-            else:
-                for word in _words:
-                    line_candidates.append(line["query"].replace("@placeholder", word))
-                pred_idx = np.random.choice(np.arange(1, len(line_candidates)),
-                                            size=1)[0]
-                pred = np.array(_words)[pred_idx]
-            res.append(pred)
-        return " ".join(res)
+                line_candidates.append(row["question"].replace("@placeholder", word))
+            pred_idx = np.random.choice(np.arange(1, len(line_candidates)),
+                                        size=1)[0]
+            pred = np.array(words)[pred_idx]
+        elif len(_words) == 1:
+            pred = _words[0]
+        else:
+            for word in _words:
+                line_candidates.append(row["question"].replace("@placeholder", word))
+            pred_idx = np.random.choice(np.arange(1, len(line_candidates)),
+                                        size=1)[0]
+            pred = np.array(_words)[pred_idx]
+        return pred
+
+    def remove_candidates_heuristic(self, row):
+        words = self._get_entities(row)
+        line_candidates = []
+        _words = []
+        for word in words:
+            if word[:-1] not in row['question']:
+                _words.append(word)
+        if len(_words) == 0:
+            for word in words:
+                line_candidates.append(row['question'].replace("@placeholder", word))
+            pred_idx = np.random.choice(np.arange(1, len(line_candidates)),
+                                        size=1)[0]
+            pred = np.array(words)[pred_idx]
+        elif len(_words) == 1:
+            pred = _words[0]
+        else:
+            for word in _words:
+                line_candidates.append(row['question'].replace("@placeholder", word))
+            pred_idx = np.random.choice(np.arange(1, len(line_candidates)),
+                                        size=1)[0]
+            pred = np.array(_words)[pred_idx]
+        return pred
+
 
 
